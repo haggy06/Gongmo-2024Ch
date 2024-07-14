2595e475f024471db8099191605c751ef1310ddc
haggy06 <baegjonghyeog80@gmail.com>
2024-07-15 02:30:12 +0900

    가재 핫픽스

--- Assets/02Script/Interact/Boss/BossInteract.cs
+++ Assets/02Script/Interact/Boss/BossInteract.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class BossInteract : EnemyInteract
 {
-    [Header("Boss Info")]
-    [SerializeField]
-    private string bossName;
-    public string BossName => bossName;
     public override void Hit(EntityType victim, float damage, bool isSkill = false)
     {
         base.Hit(victim, damage);
